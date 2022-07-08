a0bc768f0dd24d6aa105b5d59642b49bee049399
LuraMoth <fasreaperearnest@gmail.com>
2022-07-07 19:08:54 -0700

    create worldgen script

--- scripts/worldgen.gd
+++ scripts/worldgen.gd
@@ -1,16 +1,13 @@
 extends Node2D
 
+var grid_map: Array = []
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	gen_grid_map(grid_map, 5,5)
+	
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func gen_grid_map(array: Array, rows: int, collums:int):
+	
+	pass
